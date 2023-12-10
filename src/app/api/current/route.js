0d10b1a899156b3e3d4b021aa78c1cf39f484fe0
Shiv Pandey <shiv@shadowcast.io>
2023-12-10 16:44:46 +0530

    some small changes

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -7,9 +7,13 @@ export async function GET(request) {
   await connectDb();
   const authToken = request.cookies.get("authToken")?.value;
   console.log(authToken);
-  const data = jwt.verify(authToken, process.env.JWT_KEY);
-  console.log(data);
-  const user = await User.findById(data._id).select("-password");
-  console.log("test");
-  return NextResponse.json(user);
+  if (authToken) {
+    const data = jwt.verify(authToken, process.env.JWT_KEY);
+    console.log(data);
+    const user = await User.findById(data._id).select("-password");
+    console.log("test");
+    return NextResponse.json(user);
+  }else{
+    return NextResponse.json({message:"user not found",success : false});;
+  }
 }
